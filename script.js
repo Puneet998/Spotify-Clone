17114ccc187aac294aad008380d872f6d6f2cbd5
puneet998 <puneetprajapati9936@gmail.com>
2025-03-16 12:07:56 +0530

    add file

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault(); // Right-click ko disable karega
+});
+
+document.addEventListener("dblclick", function (event) {
+    event.preventDefault(); // Double-click ko disable karega
+});
